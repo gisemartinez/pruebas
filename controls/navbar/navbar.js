b60c276d7badc47d724b9a113bdb204de2bb40ed
gisemartinez <gise.cpna@gmail.com>
2013-08-12 21:21:11 -0300

    pendiente el bindeo d cada elemento

--- controls/navbar/navbar.js
+++ controls/navbar/navbar.js
@@ -5,7 +5,7 @@ steal(
 ).then(
 	function(){
 		Sigma.Control(
-				'Menu'
+				'Navbar'
 			,	{
 					defaults:{
 						view:undefined
@@ -13,8 +13,13 @@ steal(
 				}
 			,	{
 					_render_content: function(data)
-					
+					{
+						this.element.addClass("navbar navbar-inverse navbar-fixed-top")
+						this._super(data)
+						this.element.find('ul:first').addClass("nav navbar-nav")
 					}
+				,	'ul li admin'
+
 				}
 			)
 	}
