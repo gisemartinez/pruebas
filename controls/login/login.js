53e500949e81db60063efa79282465f16bfdbbd4
gisemartinez <gise.cpna@gmail.com>
2013-09-27 18:13:49 -0300

    se viene el cruce de apps

--- controls/login/login.js
+++ controls/login/login.js
@@ -6,42 +6,56 @@ steal(
 ,	'pruebas/views/login.mustache'
 ,	'pruebas/models/personajes.js'
 ,	'pruebas/fixtures/personajes.js'
+,	'pruebas/controls/navbar'
+,	'pruebas/views/topbar/navbar.mustache'
 ).then(
 	function(){
 		can.Control(
 				'login'
 			,	{
-					default:
+					defaults:
 					{
 						view:'pruebas/views/login.mustache'
 					,	view_signup:undefined
-						
+					,	onSuccess: function(data)
+						{
+							console.log(this,"Llegué bien, pero nadie me mando nada",data)
+						} 
+					,	onFaillure: function(data)
+						{
+							$('form').addClass('has-error')
+							$('form').before('<div class="alert alert-danger">'+data+'</div>')
+						}
 					
 					}
 				}
 			,	{	init: function(element,options)
 					{
-						console.log(this.options.usr_model)
+						//console.log(this.options.usr_model)
 						can.append(
 								element
 							,	can.view(options.view)
 							)
+						console.log(options)
 					}
 
 				,	'.btn click': function(element,ev)
 					{
-						
-						var data_usr
-						=this.options.usr_model
+						var self = this
+					
+
+						this.options.usr_model
 							.signin(
 									can.deparam(
 											this.element.find('form').serialize()
 										)
+								).then(
+									can.proxy(this.options.onSuccess,this)
+								,	can.proxy(this.options.onFaillure,this)
 								)
-
-						console.log(data_usr)
-
+	
 					}
+				
 				}
 			)
 
